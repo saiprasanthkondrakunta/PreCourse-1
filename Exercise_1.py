86b374c44618c953d3759bcc341e213574252b57
saiprasanthkondrakunta <kondrakunta.prasanth@gmail.com>
2022-01-27 11:25:11 -0500

    Final Exercise1.py

--- Exercise_1.py
+++ Exercise_1.py
@@ -2,20 +2,28 @@ class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
      def __init__(self):
+       self.list1 = []
          
      def isEmpty(self):
+       return self.list1 == []
+        
          
      def push(self, item):
+       self.list1.insert(0,item)
+
          
      def pop(self):
+       self.list1.pop(0)
         
         
      def peek(self):
+       return self.list1[0]
         
      def size(self):
+       return len(self.list1)
          
      def show(self):
-       print("hi")
+       return self.list1
          
 
 s = myStack()
